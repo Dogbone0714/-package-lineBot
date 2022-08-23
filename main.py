a46c93287d0b8a81022e62f4a0ff41b206d6bdae
Dogbone0714 <jameskang0714@gmail.com>
2022-08-23 19:46:41 +0800

    1

--- main.py
+++ main.py
@@ -25,11 +25,35 @@ SLEEPTIME = 60*60*24    #60*60*24 # 每輪搜尋休眠時間
 # ---------------------
 flog = False # 判斷是否已尋找到目標用的
 t = dt.datetime # 顯示時間用的
-
+# -------Mail ---------------
+''' def send_mail_for_me():
+    # '利用 Gmail 的服務寄發通知信'
+    send_gmail_user = 'nuupackage@gmail.com'
+    send_gmail_password = 'lruvafvdkrfabbkx'
+    rece_gmail_user = 'jameskang0714@gmail.com'
+    msg = MIMEText('康康~~尼的包裹已經到了收發室~~~\n記得去取件！')
+    msg['Subject'] = '聯大收發室包裹到貨通知'
+    msg['From'] = send_gmail_user
+    msg['To'] = rece_gmail_user
+    # 使用 SSL 加密 連線到 gmail 提供的 smtp
+    ssl_version=ssl.PROTOCOL_TLS
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(send_gmail_user, send_gmail_password)
+    server.send_message(msg)
+    server.quit()'''
 
 #------Line Notify------
 def lineNotifyMessage(ID, msg):
+#     headers = {
+#         "Authorization": "Bearer " + token, 
+#         "Content-Type" : "application/x-www-form-urlencoded"
+#     }
 
+#     massage = {"to": "Ud1c5291c8e4e72303076ce64aa4d15df",'message':[{"type":"text","text":msg}]}
+#     print(massage)
+#     r = requests.post("https://api.line.me/v2/bot/message/push", headers = headers, params = massage)
+#     return r.status_code
     token = 'q3MP3UJKy4sv9mWOAjkueqfJTAre7jAmyH4QPcferhiYMhds1rN9bNa6bwqHg8C4Uw22L4/4u5yyu0nVW4se0N+fcsl94olc23eP5eyUgsPRA+6LIWKrEGfJkuBIvqyG2hzY0gb3IW/qRCfseaemCAdB04t89/1O/w1cDnyilFU=' # 權杖值
     line_bot_api = LineBotApi(token)
     line_bot_api.push_message(ID, TextSendMessage(text=msg))
@@ -93,6 +117,6 @@ def main():
 # ----運行-------
 if __name__ == "__main__":
     main()
-    message = '~~尼的包裹已經到了收發室~~~\n記得去取件！' # 要傳送的訊息內容
+    message = '康康~~尼的包裹已經到了收發室~~~\n記得去取件！' # 要傳送的訊息內容
     token = 'q3MP3UJKy4sv9mWOAjkueqfJTAre7jAmyH4QPcferhiYMhds1rN9bNa6bwqHg8C4Uw22L4/4u5yyu0nVW4se0N+fcsl94olc23eP5eyUgsPRA+6LIWKrEGfJkuBIvqyG2hzY0gb3IW/qRCfseaemCAdB04t89/1O/w1cDnyilFU=' # 權杖值
     #lineNotifyMessage(token, message)
